ce3011235d18470ba497da4df52d42884886c030
Cole <letchro@gmail.com>
2022-11-29 13:20:05 -0600

    added rendering of choice answers in finished process list

--- src/app/_interfaces/IFinishedProcess.ts
+++ src/app/_interfaces/IFinishedProcess.ts
@@ -1,5 +1,5 @@
 export interface IFinishedProcess {
   id: number,
   title: string
-  finishedStages: { prompt: string, answer: string }[]
+  finishedStages: { prompt: string, answer: string, finishedChoices: { text: string, "value": string } [] }[]
 }
